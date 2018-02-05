2555f55a7dbfff9894ecd78608cc1aa96c869978
zen4orce-Ajinkya <ajinkya.kasabe@zen4orce.com>
2018-02-05 10:19:58 +0530

    Update webhook.js

--- modules/webhook.js
+++ modules/webhook.js
@@ -2,6 +2,22 @@
 
 let nforce = require('nforce');
 
+
+let nforce = require('nforce'),
+
+    SF_CLIENT_ID = process.env.SF_CLIENT_ID,
+    SF_CLIENT_SECRET = process.env.SF_CLIENT_SECRET,
+    SF_USER_NAME = process.env.SF_USER_NAME,
+    SF_PASSWORD = process.env.SF_PASSWORD;
+
+let org = nforce.createConnection({
+    clientId: SF_CLIENT_ID,
+    clientSecret: SF_CLIENT_SECRET,
+    redirectUri: 'http://localhost:3000/oauth/_callback',
+    mode: 'single',
+    autoRefresh: true
+});
+
 let request = require('request'),
     salesforce = require('./salesforce'),
     formatter = require('./formatter-messenger');
